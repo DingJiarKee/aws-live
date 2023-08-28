a11a75dc8b3c34aa3462f80f68500f99a0747f07
DingJiarKee <112339981+DingJiarKee@users.noreply.github.com>
2023-08-28 10:48:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c8osisgncekl.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "dingjiarkee-employee"
+customregion = "us-east-1"
+
